9ac37b183c1dee5974372992016948ca1fae5f8d
Revathi Mahalingam <103559154+revathimsc@users.noreply.github.com>
2023-05-26 16:55:14 +0530

    Update Blogs.js

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -6,7 +6,7 @@ const Blogs = () => {
   //get blogs
   const getAllBlogs = async () => {
     try {
-      const { data } = await axios.get("/api/v1/blog/all-blog");
+      const { data } = await axios.get("https://blog-application-backend-fsfz.onrender.com/api/v1/blog/all-blog");
       if (data?.success) {
         setBlogs(data?.blogs);
       }
