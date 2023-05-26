0a28933d25c3263be52b85ff5e0272123bf3b9a6
Revathi Mahalingam <103559154+revathimsc@users.noreply.github.com>
2023-05-26 16:57:31 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,7 +26,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("/api/v1/user/login", {
+      const { data } = await axios.post("https://blog-application-backend-fsfz.onrender.com/api/v1/user/login", {
         email: inputs.email,
         password: inputs.password,
       });
